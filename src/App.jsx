3f6ed2dc32c3a016cb0d62bb9921aaafd7509a33
chris <chriszala28@gmail.com>
2026-01-24 00:04:19 -0500

    added dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
+import { useState } from 'react';
 import Calendar from "./Calendar";
 import Checklist from "./Checklist";
 
 export default function App() {
+  const [isDark, setIsDark] = useState(false);
   return (
     <>
-      <div className='app'>
-      <h2>Dashboard</h2>
-      <Calendar />
-      <Checklist />
+      <div className={`app ${isDark ? "dark" : ""}`}>
+        <button onClick={() => setIsDark(!isDark)}>{isDark ? 'Light Mode' : 'Dark Mode'}</button>
+        <h2>Dashboard</h2>
+        <Calendar />
+        <Checklist />
       </div>
     </>
   );
